4397b1c3732711b3ec8769ef72fd9e211a194b32
Bruk Teshome <brukteshome21@gmail.com>
2024-04-12 17:31:12 +0300

    set event for the contract

--- contracts/coinFlip.sol
+++ contracts/coinFlip.sol
@@ -2,7 +2,8 @@
 pragma solidity ^0.8.17;
 
 import "@chainlink/contracts/src/v0.8/vrf/VRFV2WrapperConsumerBase.sol";
-
+event CoinFlipRequest(uint256 requestId);
+event CoinFlipResult(uint256 requestId, bool didWin);
 
 contract CoinFlip {
     enum ConFlipSelection {
